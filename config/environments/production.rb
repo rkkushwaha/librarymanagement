145178a683f8152755bc53941da539c618b51324
Raushan <kr93554.rk@gmail.com>
2022-08-26 10:17:49 +0530

    fix issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,13 +121,5 @@ Rails.application.configure do
     :host => 'localhost:3000', 
     :protocol => 'http'
     }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port: 587,
-    address: 'smtp.gmail.com',
-    user_name: 'ps7309012@gmail.com',
-    password: 'xpwoleotiaraoegz',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+
 end
